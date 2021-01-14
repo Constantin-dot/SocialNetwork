70f2a557ef9cb9b08269e7e1f3d4351549ed0230
Constantin-dot <konstantinkspb@gmail.com>
2021-01-14 18:08:12 +0300

    added reconnect and cleanup to web socket

--- src/pages/chat/ChatPage.tsx
+++ src/pages/chat/ChatPage.tsx
@@ -1,7 +1,5 @@
 import React, {useEffect, useState} from "react";
 
-const wsChannel = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
-
 export type ChatMessageType = {
     message: string
     photo: string
@@ -12,61 +10,114 @@ export type ChatMessageType = {
 
 const ChatPage: React.FC = () => {
     return <>
-        <Chat />
+        <Chat/>
     </>
 }
 
 const Chat: React.FC = () => {
+    const [wsChannel, setWsChannel] = useState<WebSocket | null>(null)
 
-    return <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", margin: "30px"}}>
-        <Messages/>
-        <ChatMessagesForm/>
+    useEffect(() => {
+        let ws: WebSocket
+        const closeHandler = () => {
+            console.log('WEB SOCKET IS CLOSED')
+            setTimeout(createChannel, 3000)
+        }
+        function createChannel() {
+            ws?.removeEventListener('close', closeHandler)
+            ws?.close()
+            ws = new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+            ws.addEventListener('close', closeHandler)
+            setWsChannel(ws)
+        }
+        createChannel()
+
+        return () =>  {
+            ws.removeEventListener('close', closeHandler)
+            ws.close()
+        }
+    }, [])
+
+    return <div style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        margin: "30px"
+    }}>
+        <Messages wsChannel={wsChannel}/>
+        <AddMessageForm wsChannel={wsChannel}/>
     </div>
 }
 
-const Messages: React.FC = () => {
+const Messages: React.FC<{wsChannel: WebSocket | null}> = ({wsChannel}) => {
     const [messages, setMessages] = useState<ChatMessageType[]>([])
+
     useEffect(() => {
-        wsChannel.addEventListener('message', (e) => {
+        let messageHandler = (e: MessageEvent) => {
             let newMessages = JSON.parse(e.data)
             setMessages((prevMessages) => [...prevMessages, ...newMessages])
-        })
-    })
+        }
+        wsChannel?.addEventListener('message', messageHandler)
 
-    return <div style={{ height: "500px", overflowY: "auto", width: "800px" }}>
+        return () => {
+            wsChannel?.removeEventListener('message', messageHandler)
+        }
+    }, [wsChannel])
+
+    return <div style={{height: "500px", overflowY: "auto", width: "800px"}}>
         {messages.map((m, index) => <Message key={index} message={m}/>)}
     </div>
 }
 
-const Message: React.FC<{message: ChatMessageType}> = ({message}) => {
+const Message: React.FC<{ message: ChatMessageType }> = ({message}) => {
     return <div>
-        <img src={message.photo} style={{width: "30px", borderRadius: "30%", marginRight: "5px"}}/>
+        <img
+            alt={'userPick'}
+            src={message.photo}
+            style={{width: "30px", borderRadius: "30%", marginRight: "5px"}}
+        />
         <b>{message.userName}</b>
         <br/>
         {message.message}
-       <hr/>
+        <hr/>
     </div>
 }
 
-const ChatMessagesForm: React.FC = () => {
+const AddMessageForm: React.FC<{wsChannel: WebSocket | null}> = ({wsChannel}) => {
     const [message, setMessage] = useState('')
+    const [readyStatus, setReadyStatus] = useState<'pending' | 'ready'>('pending')
+
+    useEffect(() => {
+        let openHandler = () => {
+            setReadyStatus('ready')
+        }
+        wsChannel?.addEventListener('open', openHandler)
+        return () => {
+            wsChannel?.removeEventListener('open', openHandler)
+        }
+    }, [wsChannel])
 
     const sendMessage = () => {
         if (!message) {
             return
         }
-        wsChannel.send(message)
+        wsChannel?.send(message)
         setMessage("")
     }
 
-    return <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", width: "800px"}}>
+    return <div style={{display: "flex", flexDirection: "column", justifyContent: "center", width: "800px"}}>
         <textarea
-            style={{ margin: "30px"}}
+            style={{margin: "30px"}}
             onChange={(e) => setMessage(e.currentTarget.value)}
             value={message}>
 
         </textarea>
-        <button onClick={sendMessage}>Send</button>
+        <button
+            disabled={wsChannel === null || readyStatus !== 'ready'}
+            onClick={sendMessage}
+        >Send
+        </button>
     </div>
 }
 export default ChatPage
